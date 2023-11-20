e85e9cf5918af4acb1dc634d6d98a74c4f274fe8
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-19 22:07:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -40,35 +40,18 @@ def shorten_link(original_link):
         print(f"Erro ao encurtar o link: {e}")
         return None
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        ip_address = request.remote_addr
-        client_ips.append(ip_address)  # Adiciona o IP à lista
-
-        # Obter informações de geolocalização
-        geo_data = get_geo_data(ip_address)
-        
-        if "Erro" in geo_data:
-            return render_template('index.html', error=geo_data)
-        
-        # Adicionando lógica para obter coordenadas geográficas precisas
-        latitude, longitude = geo_data.get('loc', '').split(',')
-        
-        # Adicionar data, hora e nome de usuário ao log
-        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        username = request.form.get('username', 'N/A')  # Substitua 'N/A' pelo valor padrão desejado
-        log_entry = f"{current_datetime} - IP: {ip_address}, Usuário: {username}, Localização: {geo_data.get('city', '')}, Latitude: {latitude}, Longitude: {longitude}"
-        
-        # Adicionar a entrada ao log
-        logging.info(log_entry)
-
-        original_link = generate_link(ip_address)
-        shortened_link = shorten_link(original_link)
-
-        return render_template('index.html', latitude=latitude, longitude=longitude, original_link=original_link, shortened_link=shortened_link)
-    else:
-        return render_template('index.html')
+def run_app():
+    port = 80
+    while True:
+        try:
+            app.run(host='0.0.0.0', port=port, debug=False)
+        except OSError as e:
+            if "Address already in use" in str(e):
+                print(f"A porta {port} já está em uso. Tentando outra porta.")
+                port += 1
+            else:
+                print(f"Erro ao iniciar o aplicativo: {e}")
+                break
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    run_app()
