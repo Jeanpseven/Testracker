f0ce7f08c9e55c8798e80b715a6291fc1b13ec6e
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-19 21:20:14 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
+echo "Instalando dependências e criando ambiente virtual..."
+
 # Nome do ambiente virtual
 venv_name="myenv"
-
-echo "Instalando dependências e criando ambiente virtual..."
 
 # Criar e ativar o ambiente virtual
 python -m venv $venv_name
 source ./$venv_name/bin/activate
 
-# Instalar o pacote GeoIP2 para Python diretamente do GitHub dentro do ambiente virtual
+# Tentar instalar o pacote GeoIP2 para Python diretamente do GitHub dentro do ambiente virtual
 pip install git+https://github.com/maxmind/GeoIP2-python.git
+
+# Verificar se a instalação foi bem-sucedida ou se há um erro
+if [ $? -ne 0 ]; then
+    echo "Erro ao instalar GeoIP2. Tentando instalar geolite2..."
+    
+    # Tentar instalar o pacote geolite2 para Python diretamente do GitHub dentro do ambiente virtual
+    pip install git+https://github.com/lelit/geolite2.git
+
+    # Verificar se a instalação foi bem-sucedida ou se há outro erro
+    if [ $? -ne 0 ]; then
+        echo "Erro ao instalar geolite2. Certifique-se de ter o python3-venv instalado e tente novamente."
+        # Sair do script com código de erro
+        exit 1
+    fi
+fi
 
 echo "Baixando o GeoLite2..."
 
@@ -19,9 +34,6 @@ download_url="https://github.com/maxmind/GeoLite2-City/releases/latest/download/
 
 # Nome do arquivo compactado
 zip_file="GeoLite2-City.mmdb.gz"
-
-# Nome do arquivo descompactado
-geoip_file="GeoLite2-City.mmdb"
 
 # Baixar o arquivo compactado
 wget $download_url
