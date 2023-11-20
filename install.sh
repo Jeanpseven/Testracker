2b475d9315d2f963c240a0fde7d78728cdba404d
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-19 21:43:20 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
 
-echo "Instalando dependências e criando ambiente virtual..."
+echo "Instalando dependências..."
 
-# Nome do ambiente virtual
-venv_name="myenv"
-
-# Criar e ativar o ambiente virtual
-python -m venv $venv_name
-source ./$venv_name/bin/activate
-
-# Tentar instalar o pacote GeoIP2 para Python diretamente do GitHub dentro do ambiente virtual
+# Tentar instalar o pacote GeoIP2 para Python diretamente do GitHub
 pip install git+https://github.com/maxmind/GeoIP2-python.git
 
 # Verificar se a instalação foi bem-sucedida ou se há um erro
 if [ $? -ne 0 ]; then
-    echo "Erro ao instalar GeoIP2. Tentando instalar geolite2..."
-    
-    # Tentar instalar o pacote geolite2 para Python diretamente do GitHub dentro do ambiente virtual
-    pip install git+https://github.com/lelit/geolite2.git
-
-    # Verificar se a instalação foi bem-sucedida ou se há outro erro
-    if [ $? -ne 0 ]; then
-        echo "Erro ao instalar geolite2. Certifique-se de ter o python3-venv instalado e tente novamente."
-        # Sair do script com código de erro
-        exit 1
-    fi
+    echo "Erro ao instalar GeoIP2. Certifique-se de ter o pip e o python3 instalados e tente novamente."
+    # Sair do script com código de erro
+    exit 1
 fi
 
 echo "Baixando o GeoLite2..."
@@ -42,6 +27,3 @@ wget --output-document=$zip_file $download_url
 gunzip $zip_file
 
 echo "GeoLite2 baixado com sucesso!"
-
-# Desativar o ambiente virtual
-deactivate
