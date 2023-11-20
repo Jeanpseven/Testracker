48e038e8b323b69a81bc3fa2110e1eca94ab8e04
W r e n c h <134978254+Jeanpseven@users.noreply.github.com>
2023-11-19 21:56:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,68 +1,81 @@
 from flask import Flask, render_template, request
-from urllib.request import urlretrieve
-import gzip
-import shutil
-import os
-from geolite2 import geolite2
-from datetime import datetime
-
-# Função para baixar e descompactar o GeoLite2
-def download_geolite2():
-    download_url = "https://github.com/maxmind/GeoLite2-City/releases/latest/download/GeoLite2-City.mmdb.gz"
-    local_path = "GeoLite2-City.mmdb.gz"
-    geoip_path = "GeoLite2-City.mmdb"
-
-    try:
-        # Baixar o arquivo compactado
-        urlretrieve(download_url, local_path)
-
-        # Descompactar o arquivo
-        with gzip.open(local_path, 'rb') as f_in, open(geoip_path, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-
-        # Remover o arquivo compactado
-        os.remove(local_path)
-
-        print("GeoLite2 baixado com sucesso!")
-
-    except Exception as e:
-        print(f"Erro durante o download/descompactação do GeoLite2: {e}")
-
-# Verificar se o arquivo GeoLite2 existe e se está desatualizado
-def is_geoip_outdated():
-    geoip_path = "GeoLite2-City.mmdb"
-
-    if not os.path.exists(geoip_path):
-        return True
-
-    # Comparar datas de modificação
-    current_time = datetime.now()
-    modified_time = datetime.fromtimestamp(os.path.getmtime(geoip_path))
-    days_difference = (current_time - modified_time).days
-
-    # Se o arquivo tem mais de 7 dias, considerar desatualizado
-    return days_difference > 7
-
-if is_geoip_outdated():
-    print("Baixando GeoLite2...")
-    download_geolite2()
+from upnpc import UPnP
+import json
+import requests
+import logging
 
 app = Flask(__name__)
+
+# Configurar o logging
+logging.basicConfig(filename='click_logs.txt', level=logging.INFO, format='%(asctime)s - %(message)s')
+
+# URL da API para obter informações de geolocalização com base no IP
+api_url = "https://ipinfo.io/json"
+
+# Lista para armazenar os IPs dos clientes que clicaram no link
+client_ips = []
+
+# Função para obter informações de geolocalização a partir da API
+def get_geo_data(ip_address):
+    try:
+        with requests.get(f"{api_url}/{ip_address}") as response:
+            geo_data = response.json()
+            return geo_data
+
+    except Exception as e:
+        return f"Erro ao obter informações de geolocalização: {e}"
+
+# Função para configurar o redirecionamento de porta usando UPnP
+def configure_upnp():
+    try:
+        upnp = UPnP()
+        upnp.discover()
+        upnp.selectigd()
+        upnp.addportmapping(80, 'TCP', upnp.lanaddr, 80, 'Flask App', '')
+        print("Redirecionamento de porta configurado com sucesso!")
+
+    except Exception as e:
+        print(f"Erro ao configurar o redirecionamento de porta UPnP: {e}")
+
+# Função para gerar o link com base no endereço IP público
+def generate_link(ip_address):
+    return f"http://{ip_address}/"
+
+# Função para encurtar o link usando TinyURL
+def shorten_link(original_link):
+    try:
+        response = requests.post("http://tinyurl.com/api-create.php?url=" + original_link)
+        short_url = response.text
+        return short_url
+
+    except Exception as e:
+        print(f"Erro ao encurtar o link: {e}")
+        return None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         ip_address = request.remote_addr
-        geo_data = geolite2.lookup(ip_address)
-        if geo_data is not None:
-            latitude = geo_data.latitude
-            longitude = geo_data.longitude
-            location = geolite2.get_name_by_ip(ip_address)
-            return render_template('index.html', latitude=latitude, longitude=longitude, location=location)
-        else:
-            return render_template('index.html', error='IP address not found.')
+        client_ips.append(ip_address)  # Adiciona o IP à lista
+
+        geo_data = get_geo_data(ip_address)
+        
+        if "Erro" in geo_data:
+            return render_template('index.html', error=geo_data)
+        
+        # Adicionando lógica para obter coordenadas geográficas precisas
+        latitude, longitude = geo_data.get('loc', '').split(',')
+        
+        original_link = generate_link(ip_address)
+        shortened_link = shorten_link(original_link)
+
+        # Log de informações
+        logging.info(f"IP: {ip_address}, Localização: {geo_data.get('city', '')}, Latitude: {latitude}, Longitude: {longitude}")
+
+        return render_template('index.html', latitude=latitude, longitude=longitude, original_link=original_link, shortened_link=shortened_link)
     else:
         return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    configure_upnp()
+    app.run(host='0.0.0.0', port=80, debug=False)
